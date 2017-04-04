17be6f12a83fe3615afd39bcb00e4df7b4555bf5
Ty <wvaugha2@vols.utk.edu>
2017-04-04 19:17:30 -0400

    blah

--- part1.py
+++ part1.py
@@ -28,9 +28,16 @@ if __name__ == "__main__":
     senders, receivers = openFile(sys.argv[1])
 
     # Print out dem bad boys
-    for s, r in zip(senders, receivers):
+    #for s, r in zip(senders, receivers):
         #print ("S: ", s)
         #print ("R: ", r)
 
-        
+    for i in range(100):
+        for j in range(32):
+            index_i = ord(senders[i][j][0]) - 97
+            index_j = int(senders[i][j][1])
+            print(index_i, index_j)
+            break;
+        break;
+
 
