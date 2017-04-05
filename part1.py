540c8ab8ff3c29b1a816e6dda0f49a6dbe28f54c
ellias palcu <epalcu@vols.utk.edu>
2017-04-04 20:11:31 -0400

    Finished placing users in dictionary with their rounds

--- part1.py
+++ part1.py
@@ -4,11 +4,17 @@ import sys
 import os
 import re
 
+def senderCheck(name):
+    if(name[0] >= 'a' and name[0] <= 'z' and name[1] == '0'):
+        return True
+    else:
+        return False
+
 def openFile(fname):
-    
+
     senders = []
     receivers = []
-    
+
     with open(fname) as fp:
         lines = fp.readlines()
 
@@ -21,23 +27,35 @@ def openFile(fname):
                 receivers.append(input)
     return senders, receivers
 
+def fill_dict(senders, receivers, dict):
+    for rnd in range(0, len(senders)):
+        for sender in senders[rnd]:
+            key = sender
+            rnd_list = [0]*260
+            for receiver in receivers[rnd]:
+                index_i = ord(receiver[0]) - 97
+                index_j = int(receiver[1])
+                offset = (index_i*10) + index_j
+                rnd_list[offset] = 1/32.0
+            if key in dict.keys():
+                value = dict.get(key)
+                value.append((rnd, rnd_list))
+            else:
+                dict[key] = [(rnd, rnd_list)]
+    return dict
+
+def print_dict(dict):
+    for key, value in dict.items():
+        print "{0} --> {1}".format(key,  value)
+
 
 if __name__ == "__main__":
-
+    users = {}
     # Get the senders and receivers... booyah!!!
     senders, receivers = openFile(sys.argv[1])
-
+    users = fill_dict(senders, receivers, users)
+    print_dict(users)
     # Print out dem bad boys
     #for s, r in zip(senders, receivers):
         #print ("S: ", s)
         #print ("R: ", r)
-
-    for i in range(100):
-        for j in range(32):
-            index_i = ord(senders[i][j][0]) - 97
-            index_j = int(senders[i][j][1])
-            print(index_i, index_j)
-            break;
-        break;
-
-
