598d7b012c5ac888d06fb4055ae0a5efc4f2dda0
ellias palcu <epalcu@vols.utk.edu>
2017-04-04 21:03:51 -0400

    Included commenting

--- part1.py
+++ part1.py
@@ -1,23 +1,15 @@
-
-
 import sys
 import os
 import re
 
-def senderCheck(user):
-    if (user[1] == '0'):
-        return True
-    else:
-        return False
-
+###################################################################################
+# Open the csv file of senders and receivers and return their corresponding lists #
+###################################################################################
 def openFile(fname):
-
     senders = []
     receivers = []
-
     with open(fname) as fp:
         lines = fp.readlines()
-
         for line in lines:
             if line[0] == 'S':
                 input = re.findall(r"(?<![@#])\b\w+(?:'\w+)?", line[1:])
@@ -27,12 +19,30 @@ def openFile(fname):
                 receivers.append(input)
     return senders, receivers
 
+#######################################################################
+# Check to make sure sender is only the 0th sender for its associated #
+# letter --> (ex: a0, b0, c0, d0, etc..)                              #
+#######################################################################
+def senderCheck(user):
+    if (user[1] == '0'):
+        return True
+    else:
+        return False
+
+#############################################################
+# Return recipient location in users list for current round #
+#############################################################
 def find_offset(user):
     index_i = ord(user[0]) - 97
     index_j = int(user[1])
     offset = (index_i*10) + index_j
     return offset
 
+###########################################################################
+# Fill the dictionary with usrs being keys, value is a list of tuples     #
+# correpsonding to current round, first element, and a list of recipients #
+# for that round, second element.                                         #
+###########################################################################
 def fill_dict(senders, receivers, dict):
     for rnd in range(0, len(senders)):
         for sender in senders[rnd]:
@@ -51,6 +61,9 @@ def fill_dict(senders, receivers, dict):
                     dict[sender] = [(rnd, rnd_list)]
     return dict
 
+########################
+# Print out users, yo. #
+########################
 def print_dict(dict):
     for key, value in dict.items():
         for item in value:
